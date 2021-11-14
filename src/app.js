987a7b46ef6bef79a91354507c70613398b63e69
WaffleCodez <73138329+WaffleCodez@users.noreply.github.com>
2021-11-14 14:20:42 +0000

    CHanged tabris ver

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
-const {Button, TextView, contentView} = require('tabris');
+// This is a simple Tabris.js app. Feel free to modify as you please.
+// Changes are saved immediately and will be available on your device after reload.
 
-contentView.append(
-  Button({
-    centerX: true, top: 100,
-    text: 'Click Me Daddy',
-    onSelect: () => {
-      $(TextView).only().text = 'mwahahahAHAHAHAHAHAHA'
-      var media = new Media('./media/nevergonnagiveyouup.mp3');
-      media.play(); 
-    }
-  }),
-  TextView({
-    centerX: true, top: 'prev() 50',
-    font: '24px'
-  })
-);
+const {Button, TextView, ui} = require('tabris');
+
+// Create a push button and add it to the content view
+let button = new Button({
+  centerX: 0, centerY: 0,
+  text: 'Click me uwu'
+}).appendTo(ui.contentView);
+
+// Create a text view and add it too
+let label = new TextView({
+  centerX: 0, bottom: [button, 20],
+  font: '24px'
+}).appendTo(ui.contentView);
+
+// Change the text when the button is pressed
+button.on('select', () => label.text = 'nigeria is kewl.');
+
+      
+      
